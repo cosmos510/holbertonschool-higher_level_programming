910e0a2ce2de1bd074de1105df78e1d57e1eef3d
cosmos510 <maximemartin510@gmail.com>
2024-05-07 09:15:07 +0000

    add authors

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # 10-add.py
+# Martin Maxime <maxime.martin@holbertonschool.com>
 
 def add(a, b):
-    """Return addition of a and b 2"""
+    """Return addition of a and b."""
     return (a + b)
