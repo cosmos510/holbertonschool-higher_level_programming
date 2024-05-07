eb915745cf22619fdc790a27e32e08e39391839d
cosmos510 <maximemartin510@gmail.com>
2024-05-07 12:18:06 +0000

    change print statement

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
 for i in range(122, 96, -1):
-    print(chr(i) if i % 2 == 0 else chr(i-32), end='')
+    if i % 2 == 0:
+        i = i
+    else:
+        i -= 32
+    print("{}".format(chr(i)), end='')
