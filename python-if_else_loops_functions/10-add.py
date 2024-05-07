8e12918cf244a18b1e7bee64850d2b02e662bdf8
cosmos510 <maximemartin510@gmail.com>
2024-05-07 09:17:39 +0000

    changing

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# 10-add.py
-# Martin Maxime <maxime.martin@holbertonschool.com>
+"""10-add.py"""
+"""Martin Maxime <maxime.martin@holbertonschool.com>"""
 
 def add(a, b):
     """Return addition of a and b."""
