061a65334e5a159279a732c0a0d38b7cb81fb670
cosmos510 <maximemartin510@gmail.com>
2024-05-07 08:35:50 +0000

    add uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    letter_up = ""
+    for letter in str:
+        if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
+            letter_up += chr(ord(letter) - 32)
+        else:
+            letter_up += letter
+    print("{letter_up}".format(letter_up))
