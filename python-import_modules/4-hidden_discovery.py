d8aaa0cb9e777b46c3bbf1f556bc30e51781d766
cosmos510 <maximemartin510@gmail.com>
2024-05-08 08:45:56 +0000

    back to normal

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
 
-import importlib.util
-import os
-
-
-module_name = "hidden_4"
-module_path = "hidden_4.pyc"
-
-spec = importlib.util.spec_from_file_location(module_name, module_path)
-hidden_4 = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(hidden_4)
-
-
-def main():
+if __name__ == "__main__":
+    import hidden_4
 
     names = dir(hidden_4)
-    non_names = sorted(name for name in names if not name.startswith("__"))
-
-    for name in non_names:
-        print(name)
-
-
-if __name__ == "__main__":
-    main()
+    for name in names:
+        if name[:2] != "__":
+            print("{}".format(name))
