28d004994e98a4b6be2fc92bd99378f1b48f6b6b
cosmos510 <maximemartin510@gmail.com>
2024-05-08 08:58:38 +0000

    back to nirmal

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,8 @@
 
 if __name__ == "__main__":
     import hidden_4
- 
+
     names = dir(hidden_4)
-    filtered_names = [name for name in names if not name.startswith("__")]
-    sorted_names = sorted(filtered_names)
-    for name in sorted_names:
-        print(name)
+    for name in names:
+        if name[:2] != "__":
+            print("{}".format(name))
