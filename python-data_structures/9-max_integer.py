562d3b889031f8901877e8956856cd03b1622f19
cosmos510 <maximemartin510@gmail.com>
2024-05-13 15:11:42 +0000

    changing max to my_list[0]

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    max = my_list[1]
+    max = my_list[0]
     for num in my_list:
         if num > max:
             max = num
