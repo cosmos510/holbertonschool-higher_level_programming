d3f43815a05335bdc6f51829743d63c5fe25ba92
cosmos510 <maximemartin510@gmail.com>
2024-05-14 14:23:58 +0000

    add condition is roman is none

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
     result = 0
     my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for letter in roman_string:
