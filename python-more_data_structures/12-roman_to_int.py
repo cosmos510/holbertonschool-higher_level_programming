9900a9bbd7c824cc177e0e830ff238c1466bab8c
cosmos510 <maximemartin510@gmail.com>
2024-05-14 15:22:58 +0000

    handle if last < value

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     result = 0
+    last = 0
     my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for letter in roman_string:
         if letter in my_dict:
-            result += my_dict[letter]
+            value = my_dict[letter]
+            result += value
+            if last < value:
+                result -= 2 * last
+            last = value
     return result
