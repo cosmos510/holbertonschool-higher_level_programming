c4c7f343e363140dd6d20085d81e58e02d1a8b47
cosmos510 <maximemartin510@gmail.com>
2024-05-16 14:08:02 +0000

    add 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script to define class.
+"""
+
+
+class Square():
+    """Class that define square
+    """
+    def __init__(self, __size = None):
+        """initiation of the class square"""
+        pass
+
