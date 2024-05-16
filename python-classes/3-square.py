e7c47701245aa2cf1731c8aee177cc43fc3c0f71
cosmos510 <maximemartin510@gmail.com>
2024-05-16 14:49:26 +0000

    add 3-square

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python script to define class.
+"""
+
+
+class Square():
+    """Class that define square
+    """
+    def __init__(self, size=0):
+        """initiation of the class square with the size"""
+
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return(self.__size * self.__size)
