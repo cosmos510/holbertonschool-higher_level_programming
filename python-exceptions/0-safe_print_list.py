4ed1381347db379f71b8ddbb25cca647e3434908
cosmos510 <maximemartin510@gmail.com>
2024-05-16 11:39:58 +0000

    fixing pyco

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,9 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end=' ')
-            ret +=1
-        except:
-            IndexError
+            ret += 1
+        except IndexError:
+            pass
     print("")
     return ret
-            
