cb46224560b7dd1730558bdf8f8fa1e18d15f5db
cosmos510 <maximemartin510@gmail.com>
2024-05-17 08:10:59 +0000

    add condition if number in tuple is positiv

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -19,6 +19,8 @@ class Square():
             raise TypeError("position must be a tuple of 2 positive integers")
         if position == ():
             raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
     def area(self):
