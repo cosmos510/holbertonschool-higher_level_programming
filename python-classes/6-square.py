932878f2e7b587f162ad9e64b2562c1550790fd3
cosmos510 <maximemartin510@gmail.com>
2024-05-17 07:43:27 +0000

    fixing blank line

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,12 +45,10 @@ class Square():
         self.__position = value
 
     def my_print(self):
+        for x in range(self.position[1]):
+            print("")
         if self.__size == 0:
             print("")
         else:
             for y in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
-        if self.__position[1] >= 0:
-            pass
-        else:
-            print(" " * self.__position[1])
