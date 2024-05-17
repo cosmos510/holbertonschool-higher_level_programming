50b9df95e4c1544831066d6d3e8cd3adf4655437
cosmos510 <maximemartin510@gmail.com>
2024-05-17 07:48:04 +0000

    adding condition if position not tuple

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -15,6 +15,9 @@ class Square():
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        if not isinstance(position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
 
     def area(self):
         """Getter method to retrieve the value of size"""
