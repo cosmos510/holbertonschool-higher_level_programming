189ebe07c5c0ff226914c4b6e70e6e538fd4b418
cosmos510 <maximemartin510@gmail.com>
2024-05-17 08:52:57 +0000

    test

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,75 +1,51 @@
 #!/usr/bin/python3
-"""Python script to define class.
-"""
+"""Module to define a Square class."""
 
+class Square:
+    """Class that defines a square."""
 
-class Square():
-    """Class that define square
-    """
     def __init__(self, size=0, position=(0, 0)):
-        """initiation of the class square with the size"""
-
-        self.__size = size
-        self.__position = position
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        if not isinstance(position, tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if position == ():
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(position[0], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(position[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = position
-
-    def area(self):
-        """Getter method to retrieve the value of size"""
-        return (self.__size * self.__size)
+        """Initialize the square with a size and a position."""
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """Getter method to retrieve the value of size"""
+        """Getter method to retrieve the value of size."""
         return self.__size
-
-    @property
-    def position(self):
-        """Getter method to retrieve the value of position"""
-        return self.__position
 
     @size.setter
     def size(self, value):
-        """Setter method to set the value of size"""
+        """Setter method to set the value of size."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Getter method to retrieve the value of position."""
+        return self.__position
+
     @position.setter
     def position(self, value):
-        """Setter method to set the value of position"""
-        if not isinstance(value, tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value == ():
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
+        """Setter method to set the value of position."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+            not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
     def my_print(self):
-        """function to print the square"""
-        for x in range(self.position[1]):
-            if self.__position[1] > 0:
-                print("")
-            else:
-                print(" ")
+        """Print the square with the '#' character."""
         if self.__size == 0:
             print("")
-        else:
-            for y in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            return
+        print("\n" * self.__position[1], end="")
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
