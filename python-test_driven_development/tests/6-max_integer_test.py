a76bbe11c0371d147d5d63b97dbeeaba0dd2b8b7
cosmos510 <maximemartin510@gmail.com>
2024-05-20 10:51:50 +0000

    add msx_integer_test

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """Define unittests for max_integer([..])."""
+    def test_ordere_list(self):
+        """Test an ordered list of integers."""
+        ordered = [1, 2, 3, 4]
+        self.assertEqual(max_integer(ordered), 4)
