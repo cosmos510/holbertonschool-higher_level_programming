aadcad5a2fbb1b2c58281171567912be653e7c1c
cosmos510 <maximemartin510@gmail.com>
2024-05-20 10:57:01 +0000

    adding more test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,6 +7,31 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """Define unittests for max_integer([..])."""
     def test_ordere_list(self):
-        """Test an ordered list of integers."""
-        ordered = [1, 2, 3, 4]
-        self.assertEqual(max_integer(ordered), 4)
+        list = [1, 2, 3, 4]
+        self.assertEqual(max_integer(list), 4)
+
+    def test_max_begiining(self):
+        list = [4, 2, 3, 1]
+        self.assertEqual(max_integer(list), 4)
+
+    def test_max_middle(self):
+        list = [1, 5, 3, 2]
+        self.assertEqual(max_integer(list), 5)
+
+    def test_negativ_num(self):
+        list = [1, -2, 3, 4]
+        self.assertEqual(max_integer(list), 4)
+
+    def test_only_negativ_num(self):
+        list = [-1, -2, -3, -4]
+        self.assertEqual(max_integer(list), -1)
+    
+    def test_one_el_in_list(self):
+        list = [4]
+        self.assertEqual(max_integer(list), 4)
+
+    def test_negativ_num(self):
+        list = []
+        self.assertEqual(max_integer(list), None)
+
+    
