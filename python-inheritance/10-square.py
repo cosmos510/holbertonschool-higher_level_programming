eeb385725d00a881f8f914408d270b428a9beb3a
cosmos510 <maximemartin510@gmail.com>
2024-05-22 12:54:39 +0000

    modifying with super()__init

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,9 +13,8 @@ class Square(Rectangle):
     def __init__(self, size):
         self.__size = size
         self.integer_validator("size", size)
+        super().__init__(size, size)
 
     def area(self):
         return self.__size * self.__size
 
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
