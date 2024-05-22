ec47662c6048b856a460c82c1d47915361c0f67b
cosmos510 <maximemartin510@gmail.com>
2024-05-22 16:16:22 +0000

    add function's doc

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -3,6 +3,9 @@
 
 
 def add_attribute(cla, name, objet):
+    """
+        Function  that adds a new attribute to an object if it’s possible
+    """
     if not hasattr(cla, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(cla, name, objet)
