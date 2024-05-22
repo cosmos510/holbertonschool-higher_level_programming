6d8e4671a498e4d62ea16fce7144c5b19a0778ab
cosmos510 <maximemartin510@gmail.com>
2024-05-22 06:53:11 +0000

    fixing pycostyle

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,6 +4,7 @@
 
 def lookup(obj):
     """
-        function that returns the list of available attributes and methods of an object
+        function that returns the list of available 
+        attributes and methods of an object
     """
     return dir(obj)
