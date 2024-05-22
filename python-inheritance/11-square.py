d4c5b87bd410389d169a646512721fbbfa9209fd
cosmos510 <maximemartin510@gmail.com>
2024-05-22 12:50:02 +0000

    add 11-square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Python script to define class.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        class that defines a square
+    """
+
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
