4b7b5b4f8495507559aaa616d336cf85b3c9a96f
cosmos510 <maximemartin510@gmail.com>
2024-05-27 15:58:50 +0000

    add read mode

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ def load_from_json_file(filename):
     """
         Function that creates an Object from a “JSON file”
     """
-    with open(filename) as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
