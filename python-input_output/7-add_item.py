4b7c3bbf3474d468b02750d5acb28aafd9b2bc6d
cosmos510 <maximemartin510@gmail.com>
2024-05-27 16:13:26 +0000

    adding try execpt

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,5 +7,10 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+try:
+    list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    list = []
 
-save_to_json_file(sys.argv[1:], "add_item.json")
+list.append(sys.argv[1:])
+save_to_json_file(list, "add_item.json")
