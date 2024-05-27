5196a968a8118d4af026cd87c33379338483abcd
cosmos510 <maximemartin510@gmail.com>
2024-05-27 09:21:34 +0000

    add from_json_string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/python3
+"""
+    Module that create function from_json_string
+"""
+from json import JSONDecoder
+
+
+def from_json_string(my_str):
+    """
+        function that returns an object
+        (Python data structure) represented by a JSON string
+    """
+    return JSONDecoder().decode(my_str)
