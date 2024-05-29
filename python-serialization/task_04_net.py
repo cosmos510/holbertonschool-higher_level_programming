8de28c3e1010ccfc3cdf205328bf29811d80f1ff
cosmos510 <maximemartin510@gmail.com>
2024-05-29 08:56:43 +0000

    add try except

--- python-serialization/task_04_net.py
+++ python-serialization/task_04_net.py
@@ -10,26 +10,31 @@ def start_server():
     """
         start a server on localhost
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        print("Received Dictionary from Client:")
-        s.bind(('localhost', 1245))
-        s.listen(1)
-        conn, addr = s.accept()
-        with conn:
-            data = conn.recv(1024)
-            print(json.loads(data.decode('utf-8')))
-            while True:
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('localhost', 1245))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
                 data = conn.recv(1024)
-                if not data:
-                    break
-                
-                print(data.decode('utf-8'))
+                print('Received Dictionary from Client:')
+                print(json.loads(data.decode('utf-8')))
+    except ConnectionError as e:
+        print(f"Connection error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
+
 
 def send_data(data):
     """
         send data to a server
-    """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(('localhost', 1245))
-        s.sendall(json.dumps(data).encode('utf-8'))
-        s.close()
+   """
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(('localhost', 1245))
+            serialized_data = json.dumps(data).encode('utf-8')
+            s.sendall(serialized_data)
+    except ConnectionError as e:
+        print(f"Connection error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
