367e19879e51fd391ac82674e730fb1eff601bb2
cosmos510 <maximemartin510@gmail.com>
2024-05-30 14:51:07 +0000

    add task_03

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+    Module that set up basic HTTP Server
+"""
+
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    """
+    Class that create a simple server
+"""
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {
+                'name': 'John Doe',
+                'age': 30,
+                'city': 'New York'
+            }
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {
+                'version': '1.0',
+                'description': 'A simple API built with http.server'
+            }
+            self.wfile.write(json.dumps(data).encode())
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(b'Endpoint not found!')
+
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
