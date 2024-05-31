d40b94c1964bde69feed6b3759168da4d46feb84
cosmos510 <maximemartin510@gmail.com>
2024-05-31 14:57:09 +0000

    add utf-8

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -11,14 +11,14 @@ import json
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     """
     Class that create a simple server
-"""
+    """
 
     def do_GET(self):
         """
         Method that handle GET request"""
         if self.path == '/':
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'Hello, this is a simple API!')
         elif self.path == '/data':
@@ -30,7 +30,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 'age': 30,
                 'city': 'New York'
             }
-            self.wfile.write(json.dumps(data).encode())
+            self.wfile.write(json.dumps(data).encode("utf-8"))
         elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
@@ -39,12 +39,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 'version': '1.0',
                 'description': 'A simple API built with http.server'
             }
-            self.wfile.write(json.dumps(data).encode())
+            self.wfile.write(json.dumps(data).encode("utf-8"))
         else:
             self.send_error(404, 'Endpoint not found')
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            self.wfile.write(b'Endpoint not found!')
 
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
