0ed2ea51a944d0f10e8fe12de0556817f93dd1c5
cosmos510 <maxime.martin510.com>
2024-06-20 12:29:22 +0000

    Refactor SQL query to include genre_id when listing all shows in the hbtn_0d_tvshows database

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT genre, COUNT(*) AS number_of_shows
+FROM hbtn_0d_tvshows
+GROUP BY genre
+HAVING COUNT(*) > 0
+ORDER BY number_of_shows DESC;
