4bd05346c0bb59615e69fdbbc5eb82d7d8eb7fe9
cosmos510 <maxime.martin510.com>
2024-06-20 12:35:54 +0000

    Refactor SQL query to fix JOIN statement when listing all genres of the show Dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 SELECT tv_genres.name
 FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genre_id = tv_show_genres.genre.id
+INNER JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows on tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
