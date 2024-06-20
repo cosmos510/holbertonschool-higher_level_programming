23a0aab8fed38ab963381c402522389421923631
cosmos510 <maxime.martin510.com>
2024-06-20 09:16:25 +0000

    Refactor SQL query to use SHOW GRANTS FOR instead of SHOW GRANTS WHERE for listing privileges of MySQL users user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SHOW GRANTS WHERE user = 'user_0d_1' OR user = 'user_0d_2';
+SHOW GRANTS FOR user = 'user_0d_1' and user = 'user_0d_2';
