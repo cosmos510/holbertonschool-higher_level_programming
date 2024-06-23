69335c91d3012298f5f76fa22e3bc29bafbcdd5e
cosmos510 <maxime.martin510.com>
2024-06-23 16:15:07 +0000

    chore: Refactor script to safely filter states by name

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-"""
-    module to list all states from the database hbtn_0e_0_usa
-"""
+"""Module compiled with Python3"""
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+def main():
+    """
+    Function that lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
+    """
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    MY_SEARCH = sys.argv[4]
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute the query
-    cursor.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC", (state_name,))
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Display the results
+    db = MySQLdb.connect(host='localhost', port=3306, user=MY_USER,
+                         passwd=MY_PASS, db=MY_DB)
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
+    cur.execute(query, (MY_SEARCH, ))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    # Close the cursor and database connection
-    cursor.close()
+    cur.close()
     db.close()
+
+
+if __name__ == '__main__':
+    main()
