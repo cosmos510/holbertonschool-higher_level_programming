1375b528e32fd127ed90ff1823dca8aa2672467e
cosmos510 <maxime.martin510.com>
2024-06-23 16:12:03 +0000

    chore: Add script to safely filter states

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    module to list all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query
+    cursor.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC", (state_name,))
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
