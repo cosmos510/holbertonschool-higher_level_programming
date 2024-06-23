8973e8590ff9b3763c356db2f3ea15efcd9f7767
cosmos510 <maxime.martin510.com>
2024-06-23 16:17:53 +0000

    Refactor script to safely filter states by name

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-"""Module compiled with Python3"""
-
+"""
+    module to list all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
-def main():
-    """
-    Function that lists all states with a name starting with N (upper N)
-    from the database hbtn_0e_0_usa
-    """
-    MY_USER = sys.argv[1]
-    MY_PASS = sys.argv[2]
-    MY_DB = sys.argv[3]
-    MY_SEARCH = sys.argv[4]
+if __name__ == "__main__":
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=MY_USER,
-                         passwd=MY_PASS, db=MY_DB)
-    cur = db.cursor()
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object
+    cursor = db.cursor()
     query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
-    cur.execute(query, (MY_SEARCH, ))
-    rows = cur.fetchall()
+    cursor.execute(query, (state_name, ))
+    rows = cursor.fetchall()
+    # Display the results
     for row in rows:
         print(row)
 
-    cur.close()
+    # Close the cursor and database connection
+    cursor.close()
     db.close()
-
-
-if __name__ == '__main__':
-    main()
